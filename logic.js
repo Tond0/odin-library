ff2b0ef519c2c09ba15c83ea51cddc5ecbfeb3ca
Tond0 <tondinialessio@gmail.com>
2026-05-10 16:35:26 +0200

    Add book + Completed Status

--- logic.js
+++ logic.js
@@ -1,46 +1,109 @@
+class Book {
+    #id;
+    #title;
+    #author;
+    #completed;
+
+    constructor(title, author, completed = false) {
+        this.#id = crypto.randomUUID();
+        this.#title = title;
+        this.#author = author;
+        this.#completed = completed;
+    }
+
+    // I metodi definiti qui finiscono automaticamente sul "Prototype", 
+    // risparmiando memoria perché non vengono ricreati per ogni singolo libro
+    GetId() { return this.#id; }
+    GetName() { return this.#title; }
+    GetAuthor() { return this.#author; }
+    GetCompleted() { return this.#completed; }
+
+    ToggleCompleted() { this.#completed = !this.#completed; }
+}
+
 let library = [];
-let libraryContainer;
+
+//HTML Elements
+//Get library
+const libraryContainer = document.querySelector("#library");
+
+//Get the add book button
+const addBookButton = document.querySelector("#addBookButton");
+
+//Form
+const inputForm = document.querySelector(".floatingForm");
+const inputBookTitle = document.querySelector("#bookTitle");
+const inputBookAuthor = document.querySelector("#bookAuthor");
+
+
 onStart();
 
-function Book(id, title, author) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-
-    this.GetId = function () {
-        return id;
-    };
-    this.GetName = function () {
-        return title;
-    };
-    this.GetAuthor = function () {
-        return author;
-    };
-}
-
 function onStart() {
-    addBookToLibrary(new Book(crypto.randomUUID(), "Animal Farm", "George Orwell"));
-    addBookToLibrary(new Book(crypto.randomUUID(), "The Little Prince", "Antoine de Saint-Exupéry"));
-    addBookToLibrary(new Book(crypto.randomUUID(), "Hunger Games", "Suzanne Collins"));
-    addBookToLibrary(new Book(crypto.randomUUID(), "Dune", "Frank Herbert"));
-    addBookToLibrary(new Book(crypto.randomUUID(), "The Great Gatsby", "F. Scott Fitzgerald"));
-    addBookToLibrary(new Book(crypto.randomUUID(), "Atomic Habits", "James Clear"));
-    addBookToLibrary(new Book(crypto.randomUUID(), "The Hobbit", "J.R.R. Tolkien"));
-    addBookToLibrary(new Book(crypto.randomUUID(), "The Midnight Library", "Matt Haig"));
-    addBookToLibrary(new Book(crypto.randomUUID(), "Sapiens", "Yuval Noah Harari"));
-    addBookToLibrary(new Book(crypto.randomUUID(), "Never Lie", "Freida McFadden"));
-    addBookToLibrary(new Book(crypto.randomUUID(), "Dark Matter", "Blake Crouch"));
+    //Add the books
+    addBookToLibrary(new Book("Animal Farm", "George Orwell", true));
+    addBookToLibrary(new Book("The Little Prince", "Antoine de Saint-Exupéry", false));
+    addBookToLibrary(new Book("Hunger Games", "Suzanne Collins", false));
+    addBookToLibrary(new Book("Dune", "Frank Herbert", false));
+    addBookToLibrary(new Book("The Great Gatsby", "F. Scott Fitzgerald", true));
+    addBookToLibrary(new Book("Atomic Habits", "James Clear", true));
+    addBookToLibrary(new Book("The Hobbit", "J.R.R. Tolkien", true));
+    addBookToLibrary(new Book("The Midnight Library", "Matt Haig", false));
+    addBookToLibrary(new Book("Sapiens", "Yuval Noah Harari", false));
+    addBookToLibrary(new Book("Never Lie", "Freida McFadden", true));
+    addBookToLibrary(new Book("Dark Matter", "Blake Crouch", false));
 
-    //Get the library container
-    libraryContainer = document.getElementById("library");
-    displayLibrary();
+    //Bind to inputForm submit event
+    inputForm.addEventListener('submit', onFormSubmit);
+
+    //Bind the add new book button
+    // addBookButton.addEventListener('click', onAddClicked);
+
+    //Display the library
+    refreshLibrary();
 }
 
+//Function is kinda useless right now, but might be a useful initialization container in the future
 function addBookToLibrary(bookToAdd) {
+    //Push in the array
     library.push(bookToAdd);
+
+    //Create a div
+    const bookDiv = createBookDiv(bookToAdd);
+
+    //Add as child
+    libraryContainer.prepend(bookDiv);
 }
 
-function displayLibrary() {
+//Factory pattern: Get a book div
+function createBookDiv(bookData) {
+    //Create the div
+    const bookTemplate = document.querySelector("#bookTemplate");
+
+    //Create a copy of the template
+    const bookClone = bookTemplate.content.cloneNode(true);
+
+    //Get Elements
+    const bookDiv = bookClone.querySelector('.book');
+    const titleSpan = bookClone.querySelector('.title');
+    const authorSpan = bookClone.querySelector('.author');
+    const btnDelete = bookClone.querySelector('#buttonDelete');
+    const btnCompleted = bookClone.querySelector('#buttonCompleted');
+
+    //Populate text
+    titleSpan.textContent = bookData.GetName();
+    authorSpan.textContent = bookData.GetAuthor();
+    btnCompleted.textContent = bookData.GetCompleted() ? "Completed" : "Not Completed";
+
+    //Bind buttons event
+    btnDelete.addEventListener('click', () => onRemoveClicked(bookData, bookDiv));
+    btnCompleted.addEventListener('click', () => onCompletedClicked(bookData, btnCompleted));
+
+    //Return the div
+    return bookDiv;
+}
+
+//Force the refresh of the entire library.
+function refreshLibrary() {
     //DEBUG
     // myLibrary.forEach((book) => {
     //     console.log(book.title);
@@ -52,33 +115,39 @@ function displayLibrary() {
     //Populate library
     library.forEach((book) => {
         //Create the div
-        const bookDiv = document.createElement('div');
-        //Add the class
-        bookDiv.classList.add('book');
-        //Construct the div with all the elements needed
-        bookDiv.innerHTML = `
-            <md-outlined-icon-button class="button-delete">
-                <md-icon>delete</md-icon>
-            </md-outlined-icon-button>
-            <div class="cover">
-                <span class="title">${book.GetName()}</span>
-                <span class="author">${book.GetAuthor()}</span>
-            </div>`;
-
-        const btnDelete = bookDiv.querySelector('.button-delete');
-        btnDelete.addEventListener('click', () => onRemoveClicked(book));
+        const bookDiv = createBookDiv(book);
 
         //Display the current book div
         libraryContainer.appendChild(bookDiv);
     });
-
 }
 
-function onRemoveClicked(bookToRemove) 
-{
+function onFormSubmit(e) {
+    //Do not reload the page
+    e.preventDefault();
+
+    //Get data
+    const title = inputBookTitle.value;
+    const author = inputBookAuthor.value;
+
+    //Create book
+    const newBook = new Book(title, author, false);
+
+    //Add to the library
+    addBookToLibrary(newBook);
+    inputForm.reset();
+}
+
+function onRemoveClicked(bookData, bookDiv) {
     //Returns an array without the book in it.
-    library = library.filter(book => book.GetId() !== bookToRemove.GetId());
+    library = library.filter(book => book.GetId() !== bookData.GetId());
 
     //Display the library without the new book
-    displayLibrary();
+    bookDiv.remove();
+}
+
+function onCompletedClicked(bookData, bookBtnCompleted) {
+    bookData.ToggleCompleted();
+    
+    bookBtnCompleted.textContent = bookData.GetCompleted() ? "Completed" : "Not Completed";
 }
